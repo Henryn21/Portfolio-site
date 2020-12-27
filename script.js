73b2af02062f3026fd9e85b621240f573b616ead
Henryn21 <45953538+Henryn21@users.noreply.github.com>
2020-12-27 00:57:08 -0800

    started flexbox

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //project carousel
 //object approach: group title, desc and tech as object values
 class project{
-    constructor(title, imgLink, tech, description){
+    constructor(title, imgLink, tech, description, demo, code){
         this.title=title;
         this.imgLink=imgLink;
         this.tech=tech;
         this.description=description;
+        this.demo=demo;
+        this.code=code;
     }
 }
 //variables, text and dom elements
@@ -17,13 +19,19 @@ let tech1="HTML, CSS, JavaScript, Google Places API"
 let tech2="HTML, CSS, JavaScript, Google Places API"
 let desc1="A recreated copy of a local taco restaurant's website";
 let desc2="An app that generates a list of local places for dates based on chosen preferences";
+let demo1="https://henryn21.github.io/taco-site/";
+let demo2="https://henryn21.github.io/Date-Generator/";
+let code1="https://github.com/Henryn21/taco-site";
+let code2="https://github.com/Henryn21/Date-Generator";
 let projectTitle=document.querySelector("#projectTitle");
 let projectImage=document.querySelector("#screenShot");
 let projectTech=document.querySelector("#techUsed");
 let projectDescription=document.querySelector("#projectDescription");
+let projectDemo=document.querySelector("#demo");
+let projectCode=document.querySelector("#code");
 //project objects
-let project1=new project(title1,img1,tech1,desc1);
-let project2=new project(title2,img2,tech2,desc2);
+let project1=new project(title1,img1,tech1,desc1,demo1,code1);
+let project2=new project(title2,img2,tech2,desc2,demo2,code2);
 let projectList=[project1,project2];
 //shows current "card"- set of project info, hides others
 function showCard(cardIndex){
@@ -31,6 +39,8 @@ function showCard(cardIndex){
     projectImage.src=projectList[cardIndex].imgLink;
     projectTech.innerHTML=projectList[cardIndex].tech;
     projectDescription.innerHTML=projectList[cardIndex].description;
+    projectDemo.href=projectList[cardIndex].demo;
+    projectCode.href=projectList[cardIndex].code;
 }
 let cardIndex=0;
 //shifts index and updates current project, direction is either left or right
